4c92e4da18d58949839138bbffbc0fcc5ce38153
v-t-9 <violeta.a.t.9@gmail.com>
2023-02-03 17:51:30 -0300

    Comentario

--- Python String Exercises.py
+++ Python String Exercises.py
@@ -1,5 +1,5 @@
 # https://pynative.com/python-string-exercise/
-import string
+# import string
 # Exercise 1A: Create a string made of the first, middle and last character
 # Write a program to create a new string made of an input string’s first, middle, and last character.
 # Given:
